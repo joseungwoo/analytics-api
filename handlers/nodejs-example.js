cca4908a8600000b64933eabaea2fa712d8c78d5
Gumball12 <kan02134@gmail.com>
2020-05-20 06:43:57 +0000

    Chore: Init working environments

--- handlers/nodejs-example.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports.workName = async (evt) => ({
-	statusCode: 200,
-	body: '안녕하세',
-});
-
--- handlers/py-example.py
+++
@@ -1,5 +0,0 @@
-def handlerName(evt, ctx):
-    return {
-        'statusCode': 404,
-        'body': '안녕하세용'
-    }
